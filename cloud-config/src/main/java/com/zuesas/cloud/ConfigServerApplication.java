4169024ed118da2fff43ded4d664f890d876bd64
pengbo <pengb@brightgrand.com>
2020-06-17 11:52:53 +0800

    first commit

---
+++ cloud-config/src/main/java/com/zuesas/cloud/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.zuesas.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringCloudApplication
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
